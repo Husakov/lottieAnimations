5251c0e45cc0804ae42946d1a665794cc5edbbfe
Samir <samhusak@hotmail.com>
2019-01-03 14:49:43 +0100

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testing';
+  public lottieConfig: Object;
+  private anim: any;
+  private animationSpeed: number = 1;
+
+  constructor() {
+    this.lottieConfig = {
+      path: 'assets/data.json',
+      autoplay: true,
+      loop: false
+    };
+  }
+
+  handleAnimation(anim: any) {
+    this.anim = anim;
+  }
+
+  stop() {
+    this.anim.stop();
+  }
+  play() {
+    this.anim.play();
+  }
+  pause() {
+    this.anim.pause();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LottieAnimationViewModule } from 'lottie-angular2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypewriterComponent } from './components/typewriter/typewriter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypewriterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LottieAnimationViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
